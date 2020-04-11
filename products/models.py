8d6afa5dd2eaf4e5a4e95a1949b35a8e30a6c41a
poyuchen <daniel7711potter@gmail.com>
2020-04-11 10:34:25 +0800

    Fixed typo

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    url = models.CharField(max_length=255)
+    votesTotal = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_concise(self):
+        return self.pub_date.strftime('%b %e, %Y')
